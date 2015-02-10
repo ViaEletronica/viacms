3758db61770206f65dc628d1952a6c12f9836578
Frank Souza <franksouza183@gmail.com>
2015-02-10 01:01:35 -0200

    Add admin plugins views

---
+++ application/views/plugins/viacms_live_search/default/viacms_live_search.php
@@ -0,0 +1,120 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+<script type="text/javascript">
+	
+	function liveSearch( cUrl, funtionOnShow ){
+		
+		if ( ! cUrl ){
+			
+			console.error( 'ViaCMS Live Search: url não especificada!' )
+			
+		}
+		else{
+			
+			$( 'input[type=text].live-search' ).each( function() {
+				
+				var elem = $(this);
+				
+				elem.attr('data-contactgrabed', true);
+				elem.attr('autocomplete', 'off');
+				
+				elem.qtip({
+					prerender: false,
+					overwrite: true,
+					content: {
+						
+						text: function(event, api) {
+							
+							api.set('content.text', '<?= lang('msg_loading'); ?>');
+							
+						}
+						
+					},
+					show: {
+						event: 'keyup',
+						effect: function(offset) {
+							
+							$(this).fadeIn( 300 ); // "this" refers to the tooltip
+							
+						},
+						delay: 700
+					},
+					events: {
+						show : function(event, api){
+							
+							// Setup the map container and append it to the tooltip
+							container = $( '<div class="s1"></div>' ).appendTo( api.elements.content.empty() );
+							
+							$.ajax({
+								type: "GET",
+								data: {
+									q: api.elements.target.val(),
+									contact_id: api.elements.target.data('contact-id'),
+									ajax: true
+								},
+								url: cUrl,
+								success: function(data){
+									
+									container.html( data );
+									
+									
+									$search_results = container.find( '.live-search-results' );
+									$search_results.prepend( '<li class="plugins-names-wrapper">' );
+									$search_results.find( '.plugin-name' ).prependTo( $search_results.find( '.plugins-names-wrapper' ) );
+									
+									
+									api.set('content.text', container);
+									
+									if ( typeof( funtionOnShow ) == 'function' ){
+										
+										funtionOnShow();
+										
+									}
+									
+								},
+								error: function(xhr, textStatus, errorThrown){
+									
+									console.log('content.text', status + ': ' + error);
+									
+									for(i in xhr){
+										if(i!="channel")
+										console.log(i + '>> ' + xhr[i]);
+									};
+									
+								}
+							});
+							
+						},
+						hide : function(event, api){
+							
+						}
+					},
+					position: {
+						target: elem,
+						viewport: $(document),
+						effect: false
+					},
+					hide: {
+						event: 'keydown unfocus',
+						effect: function(offset) {
+							$(this).fadeOut(100); // "this" refers to the tooltip
+						}
+					},
+					style: {
+						def: false,
+						classes: 'qtip-viacms-live-search live-search qtip-vecms'
+					}
+				});
+			});
+			
+		}
+		
+	}
+	
+	$( document ).bind( 'ready', function(){
+		
+		
+		
+	});
+	
+</script>
